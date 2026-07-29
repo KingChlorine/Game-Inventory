15908048c6442ca5a667334ec1e4f01dcab3d89e
KingChlorine <kingchloe281@gmail.com>
2026-07-29 11:04:46 +0100

    Add game popup window
    Gamecard onclick toggles visibility of addgame form. Display is toggled
    from none to flex. Close button on form toggles from flex to none

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Game-Inventory
 Inventory to track personal videogame collection
 Vanilla HTML, CSS and Javascript
+
+refs:
+
+coding2go CSS ONLY Carousel?
--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const searchbtn = document.getElementById('search')
 const searchbar = document.getElementById('searchbar')
 
+const card = document.getElementsByClassName('carouselcard')
+const popup = document.getElementById('overlay')
+
+const close = document.getElementById('close')
+
 function hide(){
 
-        searchbar.style.display = (searchbar.style.display === 'block') 
-        ? searchbar.style.display = 'none' : searchbar.style.display =  'block';
+        searchbar.style.display = searchbar.style.display === 'block' ? 'none' : 'block';
+
     }
+
+function popupform(){
+
+        popup.style.display = popup.style.display === 'flex' ? 'none' : 'flex';
+        
+    }
+
+function closepopup(){
+
+    popup.style.display = 'none';
+}
