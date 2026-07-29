3e52cbd6877791c48e610c967ac5575d1cace3bc
KingChlorine <kingchloe281@gmail.com>
2026-07-29 08:49:59 +0100

    Hide / Unhide searchbar
    used ternary operater to check if display is set to 'block' if yes
    change to 'none' if no change to 'block'

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const searchbar = document.getElementById('searchbar')
 
 function hide(){
 
-    searchbar.className.toggle('hide')
-
-}
+        searchbar.style.display = (searchbar.style.display === 'block') 
+        ? searchbar.style.display = 'none' : searchbar.style.display =  'block';
+    }
